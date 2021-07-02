09f3ee25f6a42232a70c3aa028757e9b10eab80c
Darpit Makwana <79750384+TechDarpit@users.noreply.github.com>
2021-07-02 15:07:56 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -3,6 +3,17 @@ import json
 import random
 import html
 
+# ANSI color codes
+Color_Off="\033[0m"     # Color Reset
+IRed="\033[0;91m"       # Intense Red
+IGreen="\033[0;92m"     # Intense Green
+IBlue="\033[0;94m"      # Intense Blue
+IWhite="\033[0;97m"     # Intense White
+BIRed="\033[1;91m"      # Bold Intense Red
+BIGreen="\033[1;92m"    # Bold Intense Green
+BIBlue="\033[1;94m"     # Bold Intense Blue
+BIWhite="\033[1;97m"    # Bold Intense White
+
 score_correct = 0
 score_incorrect = 0
 
@@ -10,46 +21,50 @@ url = "https://opentdb.com/api.php?amount=1"
 end_game = ""
 
 while end_game != "quit":
-    r = requests.get(url)
-    if(r.status_code != 200):
-        end_game =  input("Sorry, there was a problem retrieving the question. Press enter to try again or type 'quit' to quit the game.")
+    request = requests.get(url)
+    if(request.status_code != 200):
+        end_game =  input(IRed + "Sorry, there was a problem retrieving the question. Press enter to try again or type 'quit' to quit the game." + Color_Off)
     else:
         answer_validity = False
         answer_num = 1
-        data = json.loads(r.text)
+        data = json.loads(request.text)
         question = data['results'][0]['question']
         answers = data['results'][0]['incorrect_answers']
         correct_answer = data['results'][0]['correct_answer']
         answers.append(correct_answer)
         random.shuffle(answers)
 
-        print("\n" + html.unescape(question))
-
+        print("\n"+ IWhite + html.unescape(question))
         for answer in answers:
             print(chr(64 + answer_num) + ") " + html.unescape(answer))
             answer_num += 1
+        print()
 
         while answer_validity == False:
-            user_answer = str(input("\nChoose the correct answer: "))
-            user_answer = int(ord(user_answer.upper()) - 64)
-            if user_answer > len(answers) or user_answer <= 0:
-                print("Invalid answer")
-            else:
-                answer_validity = True
+            try:
+                user_answer = str(input(IWhite + "Select the correct option: " + Color_Off))
+                user_answer = int(ord(user_answer.upper()) - 64)
+                if user_answer > len(answers) or user_answer <= 0:
+                    print(IRed + "Invalid option" + Color_Off)
+                else:
+                    answer_validity = True
+            except:
+                print(IRed + "Input can't be empty. Please enter valid input." + Color_Off)
 
         user_answer = answers[int(user_answer)-1]
 
         if user_answer == correct_answer:
-            print("Congratulations! You answered correctly!")
+            print(IGreen + "Congratulations! You answered correctly!" + Color_Off)
             score_correct += 1
         else:
-            print("Sorry, " + html.unescape(user_answer) + " is incorrect. The correct answer is: " + html.unescape(correct_answer))
+            print(IWhite + "Sorry, "+ IRed + html.unescape(user_answer) + Color_Off + IWhite + " is incorrect. The correct answer is: " + IGreen + html.unescape(correct_answer) + Color_Off)
             score_incorrect += 1
 
-        end_game = input("\nPress enter to play again or type 'quit' to quit the game.").lower()
+        end_game = input(IWhite + "\nPress enter to play again or type 'quit' to quit the game." + Color_Off).lower()
 
-print("\n------ Score Board ------")
-print("Correct answers: " + str(score_correct))
-print("Incorrect answers: " + str(score_incorrect))
-print("-------------------------")
-print("\nThanks for playing 🌝")
+print(BIWhite +"\n------- Score Board -------")
+print(BIGreen +"  Correct answers   : " + str(score_correct))
+print(BIRed +"  Incorrect answers : " + str(score_incorrect))
+print(BIWhite +"--------------------------- " + Color_Off)
+
+print(BIBlue + "\nThanks for playing 🌝" + Color_Off)
